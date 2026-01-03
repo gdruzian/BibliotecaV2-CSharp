36d6e91edc2c4ef19a5f1f086c241318df14abe5
Gustavo Druzian <gustavodruzian00@gmail.com>
2026-01-03 05:53:13 -0300

    Iniciando o projeto com as primeiras classes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace BibliotecaV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringConnection = "Server=localhost;Database=biblioteca;Uid=root;Pwd=;";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(stringConnection))
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão aprovada!");
+                    Console.WriteLine("Versão: " + connection.ServerVersion);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
