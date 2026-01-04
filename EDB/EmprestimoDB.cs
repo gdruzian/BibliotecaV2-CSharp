61241ab60b9ccee4a088aa337b27b7b1b3ffeb7d
Gustavo Druzian <gustavodruzian00@gmail.com>
2026-01-04 03:07:34 -0300

    Implementando as interações dos livros com o DB

---
+++ EDB/EmprestimoDB.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using MySql.Data.MySqlClient;
+
+namespace EDB
+{
+    class EmprestimoDB
+    {
+        private string stringConnection = "Server=localhost;Database=biblioteca;Uid=root;Pwd=;";
+
+        public void CadastrarEmprestimo(int idLivro, int idUsuario)
+        {
+            try
+            {
+                using(MySqlConnection connection = new MySqlConnection(stringConnection))
+                {
+                    connection.Open();
+
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+        }
+
+    }
+}
---
+++ EDB/LivroDB.cs
@@ -0,0 +1,70 @@
+﻿using Entities;
+using MySql.Data.MySqlClient;
+
+namespace EDB
+{
+    class LivroDB
+    {
+        private string stringConnection = "Server=localhost;Database=biblioteca;Uid=root;Pwd=;";
+
+        public void CadastrarLivro(Livro livro)
+        {
+            try
+            {
+                using(MySqlConnection connection = new MySqlConnection(stringConnection))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO LIVROS (Titulo, Autor, Ano) VALUES (@Titulo, @Autor, @Ano)";
+
+                    using(MySqlCommand comando = new MySqlCommand(sql, connection))
+                    {
+                        comando.Parameters.AddWithValue("@Titulo", livro.Titulo);
+                        comando.Parameters.AddWithValue("@Autor", livro.Autor);
+                        comando.Parameters.AddWithValue("@Ano", livro.Ano);
+
+                        comando.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+
+        public void ListarLivros()
+        {
+            try
+            {
+                using(MySqlConnection connection = new MySqlConnection(stringConnection))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM LIVROS";
+
+                    using(MySqlCommand comando = new MySqlCommand(sql, connection))
+                    {
+                        using(MySqlDataReader leitor = comando.ExecuteReader())
+                        {
+                            while (leitor.Read())
+                            {
+                                string id = leitor["Id"].ToString();
+                                string autor = leitor["Autor"].ToString();
+                                string titulo = leitor["Titulo"].ToString();
+                                string ano = leitor["Ano"].ToString();
+
+                                Console.WriteLine($"{id} | {titulo} | {autor} | {ano}");
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
